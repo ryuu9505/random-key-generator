a02054c6f5996fe7b4a5d4cd030faaf7507e4f82
ryuu <ryuu.public@gmail.com>
2022-07-16 23:03:58 +0900

    FIX

---
+++ src/main/java/com/rkey/Data.java
@@ -0,0 +1,7 @@
+package com.rkey;
+
+@lombok.Data
+public class Data {
+
+    private int length;
+}
--- src/main/java/com/rkey/HomeController.java
+++ src/main/java/com/rkey/HomeController.java
@@ -2,17 +2,30 @@ package com.rkey;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(@ModelAttribute Data data, Model model) {
+
+        model.addAttribute("data", data);
+
         return "/home";
     }
+
+/*    @GetMapping("/{length}")
+    public String home(@PathVariable int length) {
+
+        System.setProperty("java.awt.headless", "false");
+        TextTransfer textTransfer = new TextTransfer();
+        String str = RandomStringUtils.randomAlphanumeric(length);
+
+        textTransfer.setClipboardContents(str);
+        return "redirect:/";
+    }*/
 
     @PostMapping("/")
     public String homeRedirect() {
