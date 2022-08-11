5d56ee61d75b1a5c49c4dcb54995874667390916
ryuu <ryuu.public@gmail.com>
2022-08-11 10:22:38 +0900

    ADD log

--- src/main/java/com/rkey/HomeController.java
+++ src/main/java/com/rkey/HomeController.java
@@ -1,16 +1,19 @@
 package com.rkey;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home(@ModelAttribute Data data, Model model) {
 
+        log.info("GetMapping to /"); // TODO
         model.addAttribute("data", data);
 
         return "home";
@@ -30,6 +33,7 @@ public class HomeController {
     @PostMapping("/")
     public String SetClipboard() {
 
+        log.info("PostMapping to /"); // TODO
         System.setProperty("java.awt.headless", "false");
         TextTransfer textTransfer = new TextTransfer();
         String str = RandomStringUtils.randomAlphanumeric(12);
